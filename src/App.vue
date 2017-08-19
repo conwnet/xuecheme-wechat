bce2b265e938ba7c7f467af37f118c4de2e2d066
netcon <netcon@live.com>
2017-08-20 06:29:56 +0800

    0819

--- src/App.vue
+++ src/App.vue
@@ -25,9 +25,13 @@
             </div>
           </div>
 
-          <mt-cell class="part" title="已选套餐" is-link @click.native="packSheetVisible = !packSheetVisible">{{ pack.name }}</mt-cell>
+          <mt-cell class="part" title="已选套餐" is-link @click.native="packSheetVisible = !packSheetVisible">
+            <mt-spinner type="double-bounce" color="#26a2ff" v-if="!pack.name"></mt-spinner>
+            {{ pack.name }}
+          </mt-cell>
 
           <div class="part panel">
+            <mt-spinner type="triple-bounce" color="#26a2ff" v-if="!pack.name"></mt-spinner>
             {{ pack.desc }}
           </div>
 
@@ -43,7 +47,16 @@
             </mt-header>
 
             <div style="margin-top: 40px;">
-              <mt-field label="姓名" placeholder="怎么称呼您？" v-model="name"></mt-field>
+              <mt-field label="微信" placeholder="获取失败，请重新进入" v-model="nickname" readonly disableClear></mt-field>
+              <mt-field label="姓名" placeholder="怎么称呼您？" v-model="user.name"></mt-field>
+              <mt-field label="手机号" placeholder="怎么联系您？" v-model="user.mobile"></mt-field>
+              <mt-field label="验证码" placeholder="请查收短信～" v-model="user.name">
+                <mt-button type="primary" @click="sendVerify()" :disabled="verifyButton.disabled">{{ verifyButton.text }}</mt-button>
+              </mt-field>
+              <mt-field label="推荐人" placeholder="推荐人的手机号" v-model="user.mobile"></mt-field>
+            </div>
+            <div class="part">
+              <mt-button style="width: 95%; margin: 0 auto;" type="primary" size="large" @click="enrollPopupVisible = true">微信支付</mt-button>
             </div>
           </mt-popup>
 
@@ -51,7 +64,22 @@
           </mt-actionsheet>
       </mt-tab-container-item>
 
+      <mt-tab-container-item id="2">
+        <div class="comments">
+            <mt-spinner type="snake" color="#26a2ff" v-if="!comments.length" :size="40" style="display: block; width: 40px; margin: 0 auto; padding: 10px;"></mt-spinner>
+            <div class="comment" v-for="(item, index) in comments" :key="index">
+              <div class="headimg"><img :src="item.headimgurl"></div>
+            </div>
+        </div>
+      </mt-tab-container-item>
+
+
     </mt-tab-container>
+
+    <mt-popup v-model="messagePopupVisible" class="messagePopup" position="top" :modal="false">
+      {{ message }}
+    </mt-popup>
+
   </div>
 </template>
 
@@ -63,13 +91,21 @@ export default {
   name: 'app',
   data () {
     return {
-      page: '1', // NavBar 的选项值
+      page: '', // NavBar 的选项值
+      messagePopupVisible: false,
+      // enroll page
       packSheetVisible: false,
+      message: '',
+      nickname: '',
       enrollPopupVisible: false,
+      user: {},
       pack: {},
       logo: {},
       packs: [],
-      banners: []
+      banners: [],
+      verifyButton: { disabled: false, text: '立即发送' },
+      // comment page
+      comments: []
     }
   },
   computed: {
@@ -81,22 +117,59 @@ export default {
       return ret
     }
   },
+  watch: {
+    page (val) {
+      if (val === '1') {
+        setTimeout(() => {
+          this.banners = [
+            { src: 'http://placehold.it/350x200', link: '#' },
+            { src: 'http://placehold.it/350x200', link: '#' }
+          ]
+          this.logo = { src: 'http://placehold.it/80x80' }
+          this.packs = [
+            { name: '2588 平民套餐', desc: '2588 平民套餐，一费到底，快速拿证！', price: 2588, id: 1 },
+            { name: '2888 奢华套餐', desc: '2888 奢华套餐，科二保过，拿证更容易！', price: 2588, id: 2 },
+            { name: '3388 土豪套餐', desc: '3388 土豪套餐，科二科三直飞，极速拿证！', price: 2588, id: 3 }
+          ]
+          this.nickname = '凌乱'
+          this.pack = this.packs[1]
+        }, 2000)
+      } else if (val === '2') {
+        setTimeout(() => {
+          this.comments = [
+            {
+              headimgurl: 'http://placehold.it/80x80',
+              nickname: '凌乱'
+            }
+          ]
+        }, 1000)
+      }
+    }
+  },
   methods: {
+    Message (message) {
+      this.messagePopupVisible = true
+      this.message = message
+      setTimeout(() => {
+        this.messagePopupVisible = false
+        this.message = ''
+      }, 2000)
+    },
+    sendVerify (message) {
+      this.Message('发送成功！')
+      let second = 60
+      this.verifyButton = { disabled: true, text: '60s' }
+      let intval = setInterval(() => {
+        this.verifyButton = { disabled: true, text: --second + 's' }
+      }, 1000)
+      setTimeout(() => {
+        clearInterval(intval)
+        this.verifyButton = { disabled: false, text: '再次发送' }
+      }, 60000)
+    }
   },
   created () {
-    setTimeout(() => {
-      this.banners = [
-        { src: 'http://placehold.it/350x150', link: '#' },
-        { src: 'http://placehold.it/350x150', link: '#' }
-      ]
-      this.logo = { src: 'http://placehold.it/80x80' }
-      this.packs = [
-        { name: '2588 平民套餐', desc: '2588 平民套餐，一费到底，快速拿证！', price: 2588, id: 1 },
-        { name: '2888 奢华套餐', desc: '2888 奢华套餐，科二保过，拿证更容易！', price: 2588, id: 2 },
-        { name: '3388 土豪套餐', desc: '3388 土豪套餐，科二科三直飞，极速拿证！', price: 2588, id: 3 }
-      ]
-      this.pack = this.packs[1]
-    }, 500)
+    this.page = '2'
   }
 }
 </script>
@@ -112,7 +185,8 @@ export default {
   }
 
   #banner {
-    height: 150px;
+    width: 100%;
+    height: 200px;
   }
 
   .panel {
@@ -132,7 +206,30 @@ export default {
 
   .part {
     margin-top: 10px;
+    background-color: #fff;
   }
 
+  .messagePopup {
+    width: 100%;
+    height: 50px;
+    text-align: center;
+    background-color: rgba(0,0,0,.7);
+    backface-visibility: hidden;
+    line-height: 50px;
+    color: #fff;
+  }
 
+  .comments {
+    background-color: #fff;
+  }
+
+  .comment .headimg {
+    width: 50px;
+    height: 50px;
+  }
+
+  .comment .headimg img {
+    width: 100%;
+    height: 100%;
+  }
 </style>
